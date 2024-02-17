6cf7704e4b79cdfadddaec9f51754dce99f8b9f6
Niyalouis <149090844+Niyalouis@users.noreply.github.com>
2024-02-17 10:11:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,39 @@
+from flask import Flask,render_template, redirect, url_for, send_file
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/analysis')
+def analysis():
+    return render_template('analysis.html')
+
+@app.route('/snp')
+def snp():
+    return render_template('snp.html')
+
+@app.route('/cluster')
+def cluster():
+    return render_template('cluster.html')
+
+@app.route('/admixture')
+def admixture():
+    return render_template('admixture.html')
+
+@app.route('/result_analysis')
+def result_analysis():
+    return render_template('result_analysis.html')
+
+@app.route('/result_snp')
+def result_snp():
+    return render_template('result_snp.html')
+
+@app.route('/download')
+def download_file():
+    p = "PCA image.png"
+    return send_file(p,as_attachment=True)
+
+if __name__=="__main__":
+    app.run(debug=True)
